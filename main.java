7e73707e357ce4333be9e38a6db01121c0373c46
cupGreek <57587329+cupGreek@users.noreply.github.com>
2023-02-14 10:37:44 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class helwrld {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        //Thread control is given to executor service.
+        server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
+        server.start();
+        System.out.println("Server started...awaiting requests");
+    }
+  
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello World! This is the thread: ";
+            long threadId = Thread.currentThread().getId();
+            System.out.println("I am thread " + threadId );
+            response = response + "Thread Id = "+threadId;
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
