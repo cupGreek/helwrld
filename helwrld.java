49e3b0a3aeba2f48e9bf89fc2f4f7ce487c998fa
cupGreek <57587329+cupGreek@users.noreply.github.com>
2023-02-27 10:44:17 +0530

    Update helwrld.java

--- helwrld.java
+++ helwrld.java
@@ -16,7 +16,7 @@ public class helwrld {
         System.out.println("Server started.....awaiting requests");
     }
     
-    // Trigger Testing - Java
+    // Trigger Testing
         
     static class MyHandler implements HttpHandler {
         @Override
